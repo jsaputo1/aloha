250ff9f7de7a6b2e9cf886acecfc65f80e384c58
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-01-26 22:18:16 -0500

    Formatting Updates

--- js/main.js
+++ js/main.js
@@ -1,85 +1,79 @@
 document.addEventListener("DOMContentLoaded", function() {
-
   //Flickity
-  
-    var elem = document.querySelector(".carousel");
-    var flkty = new Flickity(elem, {
-      // options
-      cellAlign: "left",
-      contain: true
-    });
-  
-    // element argument can be a selector string
-    //   for an individual element
-    var flkty = new Flickity(".carousel", {
-      // options
-    });
-  
-    //End of Flickity
-    
-  //Subscribe button
-  
-  document.getElementById("submit-button")
-  .addEventListener("click", function(event) {
-    event.preventDefault();
-  
-    let emailField = document.getElementById("email-field");
-      if (validateEmail(emailField.value)) {
-      alert("Thanks for subscribing!");
-      emailField.value = "";
-    } else {
-      alert("Invalid email address.");
-    }
+
+  var elem = document.querySelector(".carousel");
+  var flkty = new Flickity(elem, {
+    // options
+    cellAlign: "left",
+    contain: true
   });
-  
+
+  // element argument can be a selector string
+  //   for an individual element
+  var flkty = new Flickity(".carousel", {
+    // options
+  });
+
+  //End of Flickity
+
+  //Subscribe button
+
+  document
+    .getElementById("submit-button")
+    .addEventListener("click", function(event) {
+      event.preventDefault();
+
+      let emailField = document.getElementById("email-field");
+      if (validateEmail(emailField.value)) {
+        alert("Thanks for subscribing!");
+        emailField.value = "";
+      } else {
+        alert("Invalid email address.");
+      }
+    });
+
   function validateEmail(email) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
   }
-   
 
-  //Add to cart 
+  //Add to cart
 
   let cartText = document.getElementById("cartCount");
   let button = document.getElementById("cart-button"),
-  count = 0;
-button.onclick = function() {
-  count ++;
+    count = 0;
+  button.onclick = function() {
+    count++;
     cartText.innerHTML = "Items: " + count;
-    console.log(count)
-};
+    console.log(count);
+  };
 
-// End of add to cart
-  
-// Stick Header
-window.onscroll = function() {myFunction()};
+  // End of add to cart
 
-const header = document.getElementById("myHeader");
+  // Stick Header
+  window.onscroll = function() {
+    myFunction();
+  };
 
-const sticky = header.offsetTop;
+  const header = document.getElementById("myHeader");
 
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
+  const sticky = header.offsetTop;
+
+  function myFunction() {
+    if (window.pageYOffset > sticky) {
+      header.classList.add("sticky");
+    } else {
+      header.classList.remove("sticky");
+    }
   }
-} 
 
-// End of Sticky Header
+  // End of Sticky Header
 
-// Smooth Scrolling
+  // Smooth Scrolling
 
-window.scroll({
-  top: 2500, 
-  left: 0, 
-  behavior: 'smooth'
-});
-      
+  window.scroll({
+    top: 2500,
+    left: 0,
+    behavior: "smooth"
   });
-
-
-
-
-
-  
+});
